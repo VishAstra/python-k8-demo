5f7d88fe16464b3d3b2de94ca28bf658bf9b0562
Vishnu Chandran <vshnu911@gmail.com>
2023-08-25 23:16:30 +0530

    updated the command line

--- docker_minikube_install.sh
+++ docker_minikube_install.sh
@@ -7,11 +7,12 @@ apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo systemctl start docker
 sudo systemctl status docker
-sudo usermod -aG docker $USER
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo apt install qemu-kvm libvirt-daemon-system bridge-utils
+sudo usermod -aG docker $USER
 sudo systemctl restart docker
+minikube start --driver=docker
 docker info
 minikube start -y
 minikube kubectl -- get po -A
